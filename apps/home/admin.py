f482d9cdade170368df10e3e7fa9330f2b1efd6e
Trinitee <107314939+TriniteeKay@users.noreply.github.com>
2022-11-15 18:54:22 +0100

    Update admin.py

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import loading, withdraw, notification
+from .models import aza, loading, withdraw, notification
 
 admin.site.register(loading)
 admin.site.register(withdraw)
 admin.site.register(notification)
+admin.site.register(aza)
